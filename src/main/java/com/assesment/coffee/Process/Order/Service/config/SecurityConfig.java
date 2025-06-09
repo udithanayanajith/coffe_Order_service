bab65e6945d980a84060d7b21001d8972c80f133
udithanayanajith <udithanayanajith97@gmail.com>
2025-06-09 23:07:21 +0530

    edit testCases

--- src/main/java/com/assesment/coffee/Process/Order/Service/config/SecurityConfig.java
+++ src/main/java/com/assesment/coffee/Process/Order/Service/config/SecurityConfig.java
@@ -3,10 +3,12 @@ package com.assesment.coffee.Process.Order.Service.config;
 import com.assesment.coffee.Process.Order.Service.util.JwtUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -17,6 +19,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
  * Configures JWT authentication filter and disables CSRF protection.
  * Sets up stateless session management.
  */
+@Profile("!test")
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -38,7 +42,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/api/v1/orders/**").authenticated()
